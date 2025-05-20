ea1ede7fcef689bed620d508335233b409bc3335
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-20 14:37:31 +0200

    #1 Fix home route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::permanentRedirect('/', route('dashboard'));
+Route::permanentRedirect('/', '/dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
