b688d811c14409e88608d424832d070bc0164679
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-20 20:29:11 +0200

    #3 Add unique index on projects `user_id` and `name` column

--- database/migrations/2025_05_20_113824_create_projects_table.php
+++ database/migrations/2025_05_20_113824_create_projects_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('name');
 
             $table->timestamps();
+
+            $table->unique(['user_id', 'name']);
         });
     }
 
