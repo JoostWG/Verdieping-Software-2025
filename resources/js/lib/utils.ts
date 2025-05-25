8a820459541a3ae4888101d8b0fca56da6b9409d
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-25 17:48:54 +0200

    #22 Add `typedEntries` util function

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function typedEntries<T extends object, K extends keyof T>(o: T): [K, T[K]][] {
+    return Object.entries(o) as [K, T[K]][];
+}
