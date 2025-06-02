ffb0df2a2afebc345c74b06812a7bc68d28a1db2
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-06-02 17:42:59 +0200

    #6 Add `visitParams` and `filterMultiple` util functions

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,4 +1,6 @@
+import { router } from '@inertiajs/react';
 import { type ClassValue, clsx } from 'clsx';
+import type { VisitOptions } from 'node_modules/@inertiajs/core/types/types';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +9,22 @@ export function cn(...inputs: ClassValue[]) {
 
 export function typedEntries<T extends object, K extends keyof T>(o: T): [K, T[K]][] {
     return Object.entries(o) as [K, T[K]][];
+}
+
+export function visitParams(params: URLSearchParams, options?: VisitOptions) {
+    router.visit(`${location.origin}${location.pathname}?${params.toString()}`, options);
+}
+
+export function filterMultiple(pairs: Record<string, string>, options?: VisitOptions) {
+    const params = new URLSearchParams(location.search);
+
+    for (const [key, value] of Object.entries(pairs)) {
+        if (value === null) {
+            params.delete(key);
+        } else {
+            params.set(key, value);
+        }
+    }
+
+    visitParams(params, { replace: true, ...(options ?? {}) });
 }
